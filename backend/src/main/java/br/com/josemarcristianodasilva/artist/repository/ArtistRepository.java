979e1ddf4193965bfc7c9fa280d10463f77814fa
Josemar Cristiano da Silva <mundociberneticoti@gmail.com>
2026-01-27 16:47:32 -0400

    feat(api): add artist repository

---
+++ backend/src/main/java/br/com/josemarcristianodasilva/artist/repository/ArtistRepository.java
@@ -0,0 +1,11 @@
+package br.com.josemarcristianodasilva.artist.repository;
+
+import br.com.josemarcristianodasilva.artist.domain.Artist;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
+
+    Page<Artist> findByNameContainingIgnoreCase(String name, Pageable pageable);
+}
